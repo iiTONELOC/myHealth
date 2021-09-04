e96a59d112ba9aa07510ba2762e2097c5aef71aa
Anthony Tropeano <anthonytropeano@me.com>
2021-09-04 10:17:59 -0400

    need to drop db

--- server/seeders/delete.js
+++ server/seeders/delete.js
@@ -1,17 +1,17 @@
 const db = require('../config/connection');
 const { User, BloodPressure, DailyReading } = require('../models');
 
-if (process.env.NODE_ENV !== 'production') {
-    db.once('open', async () => {
-        try {
-            await User.deleteMany({});
-            await BloodPressure.deleteMany({});
-            await DailyReading.deleteMany({})
-        } catch (error) {
-            throw new Error('Unable to delete Database')
-        }
-        console.log('all done!');
-        process.exit(0);
-    });
-}
+
+db.once('open', async () => {
+    try {
+        await User.deleteMany({});
+        await BloodPressure.deleteMany({});
+        await DailyReading.deleteMany({})
+    } catch (error) {
+        throw new Error('Unable to delete Database')
+    }
+    console.log('all done!');
+    process.exit(0);
+});
+
 
